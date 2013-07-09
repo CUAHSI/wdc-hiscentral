84dd330f3ec02211098d2d09cb092d89303b11e2
Yaping Xiao <ypxiao2009@gmail.com>
2013-07-09 00:06:27 -0400

    code for querying WaterML 1.0

---
+++ MultiEndpoint_1_0/MultiEndpoint_1_0/Program-hiscentral.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+using MultiEndpoint_1_0.WaterML10;
+using MultiEndpoint_1_0.hiscentralDataSetTableAdapters;
+
+namespace MultiEndpoint_1_0
+{
+    class hiscentralSeriesCatalog
+    {
+        public SeriesCatalogTableAdapter Adapter;
+        public hiscentralDataSet.SeriesCatalogDataTable Table;
+
+        public hiscentralSeriesCatalog(SqlConnection sqlConn)
+        {
+            Adapter = new SeriesCatalogTableAdapter();
+            Table = new hiscentralDataSet.SeriesCatalogDataTable();
+            Adapter.Connection = sqlConn;
+        }
+
+        /*
+         * This routine is to add SQL WHERE clause to limit records to be loaded.
+         * Be sure to copy this routine to SeriesCatalogTableAdapter class when
+         * hiscentralDataSet.Designer.cs is regenerated to make GetRow() work.
+         * Copied from http://www.codeproject.com/Articles/17324/Extending-TableAdapters-for-Dynamic-SQL.
+        public int FillWhere(hiscentralDataSet.SeriesCatalogDataTable dataTable, string whereExpression)
+        {
+            string text1 = this._commandCollection[0].CommandText;
+            try
+            {
+                this._commandCollection[0].CommandText += " WHERE " + whereExpression;
+                return this.Fill(dataTable);
+            }
+            finally { this._commandCollection[0].CommandText = text1; }
+        }
+        */
+
+
+        public hiscentralDataSet.SeriesCatalogRow GetRow(string siteCode, string varCode,
+            WaterML10.seriesCatalogTypeSeries scts)
+        {
+            string cond = "SiteCode = '" + siteCode + "' and ";
+            cond += "VariableCode = '" + varCode + "'";
+
+            Adapter.FillWhere(Table, cond);
+            if (Table.Rows.Count == 0)
+                return null;
+            else
+            {
+                if (Table.Rows.Count > 1)
+                {
+                    string filter = string.Format("SeriesCode like '%||{0}||{1}||{2}'",
+                        scts.Method.methodID, scts.Source.sourceID,
+                        scts.QualityControlLevel.QualityControlLevelID);
+                    DataRow[] rows = Table.Select(filter);
+                    if (rows.Length == 1)
+                        return (hiscentralDataSet.SeriesCatalogRow)rows[0];
+                    else
+                    {
+                        Console.WriteLine(
+                          "SeriesCatalog table contains {0} records for site {1} var {2}, got {3} records with filter '{4}'.",
+                          Table.Rows.Count, siteCode, varCode, rows.Length, filter);
+                    }
+                }
+                return (hiscentralDataSet.SeriesCatalogRow)Table.Rows[0];
+            }
+        }
+    }
+}
